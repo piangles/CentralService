64d4336c05ee4743e4774b331bf8debcc172f174
saradhivs <saradhivs@yahoo.com>
2020-12-28 10:39:46 -0500

    Removed hard coded values.

--- src/main/java/org/piangles/central/services/dao/DBProperties.java
+++ src/main/java/org/piangles/central/services/dao/DBProperties.java
@@ -4,14 +4,19 @@ import java.util.Properties;
 
 public class DBProperties
 {
+	private static final String CENTRAL_DB_DRIVER = "central.db.driver";
+	private static final String CENTRAL_DB_LOGIN = "central.db.login";
+	private static final String CENTRAL_DB_PASSWORD = "central.db.password";
+	private static final String CENTRAL_DB_URL = "central.db.url";
+
 	public static Properties getDBProperties()
 	{
 		Properties dataStoreProps = new Properties();
-		dataStoreProps.put(DataStore.DRIVER_CLASSNAME, "com.mysql.jdbc.Driver");
-		dataStoreProps.put(DataStore.LOGIN_ID, "kj5IPCk6oWkmxezE9yWh3Q==");
-		dataStoreProps.put(DataStore.PASSWORD, "bSU2ekVvuinOQTa/ywH7kg==");
-		dataStoreProps.put(DataStore.URL, "jdbc:mysql://elektos.cty8sfgs1evu.us-east-1.rds.amazonaws.com:3306/");
-		
+		dataStoreProps.put(DataStore.DRIVER_CLASSNAME, System.getenv(CENTRAL_DB_DRIVER));
+		dataStoreProps.put(DataStore.LOGIN_ID, System.getenv(CENTRAL_DB_LOGIN));
+		dataStoreProps.put(DataStore.PASSWORD, System.getenv(CENTRAL_DB_PASSWORD));
+		dataStoreProps.put(DataStore.URL, System.getenv(CENTRAL_DB_URL));
+
 		return dataStoreProps;
 	}
 }
